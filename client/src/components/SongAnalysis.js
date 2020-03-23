b2c7e358e0b40034af5d899d9f7a3b30855b9eb8
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-23 17:28:38 -0400

    fixed song-list ui

--- client/src/components/SongAnalysis.js
+++ client/src/components/SongAnalysis.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Chart } from "react-google-charts";
 
 const SongAnalysis = props => {
   const [lyrics, setLyrics] = useState("");
@@ -46,16 +47,43 @@ const SongAnalysis = props => {
         </div>
         <div id="analysis">
           <h1>Analysis</h1>
-          <div className="text-left">
-            <div id="sentiment">
+          <div className="d-flex flex-row justify-content-center">
+            <div id="sentiment" className="p-2">
               <h2>Sentiment</h2>
-              <p>{`Label: ${label}`}</p>
-              <p>{`Score:  ${Number(score).toFixed(2)}%`}</p>
+              <h3>{`Label: ${label}`}</h3>
+              <h3>{`Score:  ${Number(score).toFixed(2)}%`}</h3>
             </div>
-            <div id="concepts"></div>
-            <ul id="emotion">{emotion}</ul>
+            <div className="p-2">
+              <ul id="emotion">{emotion}</ul>
+            </div>
           </div>
         </div>
+        <Chart
+          width={"500px"}
+          height={"300px"}
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ["City", "2010 Population", "2000 Population"],
+            ["New York City, NY", 8175000, 8008000],
+            ["Los Angeles, CA", 3792000, 3694000],
+            ["Chicago, IL", 2695000, 2896000],
+            ["Houston, TX", 2099000, 1953000],
+            ["Philadelphia, PA", 1526000, 1517000]
+          ]}
+          options={{
+            title: "Population of Largest U.S. Cities",
+            chartArea: { width: "50%" },
+            isStacked: true,
+            hAxis: {
+              title: "Total Population",
+              minValue: 0
+            },
+            vAxis: {
+              title: "City"
+            }
+          }}
+        />
       </div>
     );
   } else {
